147870086181073c843361a70c52081c493c1d25
Julian Podbersich <jgpodber@gmail.com>
2025-08-29 17:43:49 -0300

    Added JWT Settings.

---
+++ PTR.TPFinal.Domain/Auth/AuthenticationRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PTR.TPFinal.Domain.Auth
+{
+    public class AuthenticationRequestDto
+    {
+        public string ECommerceName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
