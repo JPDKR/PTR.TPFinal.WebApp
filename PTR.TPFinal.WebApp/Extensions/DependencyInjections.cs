2d6cfab3fe02498e37bcf00b0665707a65001130
Julian Podbersich <jgpodber@gmail.com>
2025-08-29 17:42:47 -0300

    Added extensions for both DI and JWT.

---
+++ PTR.TPFinal.WebApp/Extensions/DependencyInjections.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using PTR.TPFinal.Domain.Data;
+using PTR.TPFinal.Services.Repositories.Implementations;
+using PTR.TPFinal.Services.Repositories.Interfaces;
+using PTR.TPFinal.Services.Validators;
+using PTR.TPFinal.Services.DTOs.Requests;
+using PTR.TPFinal.Services.Interfaces;
+using PTR.TPFinal.Services.Services;
+
+namespace PTR.TPFinal.WebApp.Extensions
+{
+    public static class DependencyInjections
+    {
+        public static IServiceCollection AddDatabase(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ECommerceApiContext>(options => options.UseSqlServer(configuration.GetConnectionString("ECommerceAPI")));
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAreaRepository, AreaRepository>();
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            //services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddValidators(this IServiceCollection services)
+        {
+            services.AddFluentValidationAutoValidation();
+
+            services.AddScoped<IValidator<CreateProductRequestDto>, CreateProductRequestDtoValidator>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAreaService, AreaService>();
+            services.AddScoped<IClientService, ClientService>();
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<IProductService, ProductService>();
+            //services.AddScoped<IShoppingCartService, ShoppingCartService>();
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+}
---
+++ PTR.TPFinal.WebApp/Extensions/ServiceExtensions.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using PTR.TPFinal.Services.Models.Security;
+using System.Reflection;
+using System.Text;
+
+namespace PTR.TPFinal.WebApp.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSection = configuration.GetSection("Jwt");
+            services.Configure<JwtSettings>(jwtSection);
+
+            var jwtSettings = jwtSection.Get<JwtSettings>();
+            var key = Encoding.UTF8.GetBytes(jwtSettings!.Key);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddSwaggerWithJwt(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "PTR Web App",
+                    Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "v1",
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Ingrese el token JWT."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
